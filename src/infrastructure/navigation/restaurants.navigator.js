7fe653e3cb4d69755df0e89f72e802f8e8b389b9
Halil <halil.seran.1135@gmail.com>
2022-01-28 23:33:39 +0300

    modalDone

--- src/infrastructure/navigation/restaurants.navigator.js
+++ src/infrastructure/navigation/restaurants.navigator.js
@@ -1,11 +1,11 @@
 import React from "react";
-import { Text } from "react-native";
 import {
   createStackNavigator,
   TransitionPresets,
 } from "@react-navigation/stack";
 
 import { RestaurantsScreen } from "../../features/restaurants/screens/restaurants.screen";
+import { RestaurantDetailScreen } from "../../features/restaurants/screens/restaurant-detail.screen";
 
 const RestaurantsStack = createStackNavigator();
 
@@ -25,7 +25,7 @@ export const RestaurantsNavigator = () => {
       />
       <RestaurantsStack.Screen
         name="RestaurantDetail"
-        component={() => <Text>Restaurant Detail</Text>}
+        component={RestaurantDetailScreen}
       />
     </RestaurantsStack.Navigator>
   );
