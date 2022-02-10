ee6e312bdd90644e20e5801a9899803e8bfbc168
Halil <halil.seran.1135@gmail.com>
2022-02-10 14:24:14 +0300

    FavouritesBugFixed

--- src/infrastructure/navigation/app.navigator.js
+++ src/infrastructure/navigation/app.navigator.js
@@ -6,7 +6,11 @@ import { Text, Button } from "react-native";
 import { SafeArea } from "../../components/utility/safe-area.component";
 import { RestaurantsNavigator } from "./restaurants.navigator";
 import { MapScreen } from "../../features/map/screens/map.screen";
+
 import { AuthenticationContext } from "../../services/authentication/authentication.context";
+import { RestaurantsContextProvider } from "../../services/restaurants/restaurants.context";
+import { LocationContextProvider } from "../../services/location/location.context";
+import { FavouritesContextProvider } from "../../services/favourites/favourites.context";
 
 const Tab = createBottomTabNavigator();
 
@@ -40,9 +44,15 @@ const createScreenOptions = ({ route }) => {
 //<tabNavigator> icinde screen options i ayarlamak yerine burda ayarladik
 
 export const AppNavigator = () => (
-  <Tab.Navigator screenOptions={createScreenOptions}>
-    <Tab.Screen name="Restaurants" component={RestaurantsNavigator} />
-    <Tab.Screen name="Map" component={MapScreen} />
-    <Tab.Screen name="Settings" component={Settings} />
-  </Tab.Navigator>
+  <FavouritesContextProvider>
+    <LocationContextProvider>
+      <RestaurantsContextProvider>
+        <Tab.Navigator screenOptions={createScreenOptions}>
+          <Tab.Screen name="Restaurants" component={RestaurantsNavigator} />
+          <Tab.Screen name="Map" component={MapScreen} />
+          <Tab.Screen name="Settings" component={Settings} />
+        </Tab.Navigator>
+      </RestaurantsContextProvider>
+    </LocationContextProvider>
+  </FavouritesContextProvider>
 );
--- src/services/favourites/favourites.context.js
+++ src/services/favourites/favourites.context.js
@@ -1,26 +1,26 @@
-import React, { createContext, useState, useEffect } from "react";
+import React, { createContext, useState, useEffect, useContext } from "react";
 
 import AsyncStorage from "@react-native-async-storage/async-storage";
-
+import { AuthenticationContext } from "../authentication/authentication.context";
 
 export const FavouritesContext = createContext();
 
-
 export const FavouritesContextProvider = ({ children }) => {
+  const { user } = useContext(AuthenticationContext);
   const [favourites, setFavourites] = useState([]);
-  
-  const saveFavourites = async (value) => {
+
+  const saveFavourites = async (value, uid) => {
     try {
       const jsonValue = JSON.stringify(value);
-      await AsyncStorage.setItem("@favourites", jsonValue);
+      await AsyncStorage.setItem(`@favourites-${uid}`, jsonValue);
     } catch (e) {
       console.log("error storing", e);
     }
   };
-  
-  const loadFavourites = async () => {
+
+  const loadFavourites = async (uid) => {
     try {
-      const value = await AsyncStorage.getItem("@favourites");
+      const value = await AsyncStorage.getItem(`@favourites-${uid}`);
       if (value !== null) {
         setFavourites(JSON.parse(value));
       }
@@ -28,34 +28,38 @@ export const FavouritesContextProvider = ({ children }) => {
       console.log("error loading", e);
     }
   };
-  
+
   const add = (restaurant) => {
     setFavourites([...favourites, restaurant]);
   };
-  
+
   const remove = (restaurant) => {
     const newFavourites = favourites.filter(
       (x) => x.placeId !== restaurant.placeId
-      );
-      setFavourites(newFavourites);
-    };
-    
-    useEffect(() => {
-      loadFavourites();
-    }, []);
-    
-    useEffect(() => {
-      saveFavourites(favourites);
-    }, [favourites]);
-    
-    return (
-      <FavouritesContext.Provider
+    );
+    setFavourites(newFavourites);
+  };
+
+  useEffect(() => {
+    if (user) {
+      loadFavourites(user.uid);
+    }
+  }, [user]);
+
+  useEffect(() => {
+    if (user) {
+      saveFavourites(favourites, user.uid);
+    }
+  }, [favourites, user]);
+
+  return (
+    <FavouritesContext.Provider
       value={{
         favourites,
         addToFavourites: add,
         removeFromFavourites: remove,
       }}
-      >
+    >
       {children}
     </FavouritesContext.Provider>
   );
